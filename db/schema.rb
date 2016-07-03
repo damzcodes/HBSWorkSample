782fc8f8701732485ae7d5ead74320a267fd4641
Damilola Odelola <damzcodes@gmail.com>
2016-07-03 18:20:17 +0100

    Rename column from messages to text

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160703170202) do
+ActiveRecord::Schema.define(version: 20160703171742) do
 
   create_table "messages", force: :cascade do |t|
-    t.text     "message"
+    t.text     "text"
     t.string   "author"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
